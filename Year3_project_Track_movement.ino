2a7a83b78fbb04fd89f4b472403f1e83febc7ece
Edwhite088 <151928696+Edwhite088@users.noreply.github.com>
2025-03-19 19:33:23 +0000

    Update Year3_project_Track_movement.ino
    Added some comments

--- Year3_project_Track_movement.ino
+++ Year3_project_Track_movement.ino
@@ -1,16 +1,16 @@
-int in1 = 3, in2 = 4, enA = 5, in3 = 6, in4 = 7, enB = 9;
+int in1 = 3, in2 = 4, enA = 5, in3 = 6, in4 = 7, enB = 9; // Different motor pins
 
 void setup() {
   pinMode(in1, OUTPUT), pinMode(in2, OUTPUT), pinMode(enA, OUTPUT); //Motor One
   pinMode(in3, OUTPUT), pinMode(in4, OUTPUT), pinMode(enB, OUTPUT); //Motor Two
 }
 
-void loop() {
-  Serial.begin(9600);
-  if(Serial.available() > 0){
-    int Direction = Serial.read();
-    switch(Direction){
-      case 'W':
+void loop() { // Start the loop
+  Serial.begin(9600); // Connect to serial for the use of bluetooth
+  if(Serial.available() > 0){ // Check if the serial is connected
+    int Direction = Serial.read(); // Read from the serial
+    switch(Direction){ // Get which direction to go
+      case 'W': 
       Forward();
       break;
 
@@ -26,7 +26,7 @@ void loop() {
       Right();
       break;
 
-      default:
+      default: // Make sure the robot stops moving when nothing is sent across
       Stop();
       break;
     }
@@ -34,26 +34,26 @@ void loop() {
 }
 
 void Forward(){
-  digitalWrite(in1, HIGH), digitalWrite(in2, LOW), analogWrite(enA, 255);
+  digitalWrite(in1, HIGH), digitalWrite(in2, LOW), analogWrite(enA, 255); // Turn both motors on and forward
   digitalWrite(in3, HIGH), digitalWrite(in4, LOW), analogWrite(enB, 255);
 }
 
 void Reverse(){
-  digitalWrite(in1, LOW), digitalWrite(in2, HIGH), analogWrite(enA, 255);
+  digitalWrite(in1, LOW), digitalWrite(in2, HIGH), analogWrite(enA, 255); // Turn both motors on and backwards
   digitalWrite(in3, LOW), digitalWrite(in4, HIGH), analogWrite(enB, 255);
 }
 
 void Left(){
-  digitalWrite(in1, LOW), digitalWrite(in2, HIGH), analogWrite(enA, 255);
+  digitalWrite(in1, LOW), digitalWrite(in2, HIGH), analogWrite(enA, 255); // Turn both motors on left backwards right forwards
   digitalWrite(in3, HIGH), digitalWrite(in4, LOW), analogWrite(enB, 255);
 }
 
 void Right(){
-  digitalWrite(in1, HIGH), digitalWrite(in2, LOW), analogWrite(enA, 255);
+  digitalWrite(in1, HIGH), digitalWrite(in2, LOW), analogWrite(enA, 255); // Turn both motors on left forward right backwards
   digitalWrite(in3, LOW), digitalWrite(in4, HIGH), analogWrite(enB, 255);
 }
 
 void Stop(){
-  digitalWrite(in1, LOW), digitalWrite(in2, LOW), analogWrite(enA, 255);
+  digitalWrite(in1, LOW), digitalWrite(in2, LOW), analogWrite(enA, 255); // Turn both motors off
   digitalWrite(in3, LOW), digitalWrite(in4, LOW), analogWrite(enB, 255);
 }
